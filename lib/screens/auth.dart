ae3f61665c3f5ab3449f3a2dc08d21a999551eda
sajal <sajalsahu8@gmail.com>
2023-07-23 17:36:22 +0400

    configuerd password obs

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -8,54 +8,89 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  var passwordshown = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primary,
       body: Center(
-        child: SingleChildScrollView(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              margin: const EdgeInsets.only(
-                bottom: 20,
-                top: 30,
-                left: 20,
-                right: 20,
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: SingleChildScrollView(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(
+                  bottom: 20,
+                  top: 30,
+                  left: 20,
+                  right: 20,
+                ),
+                width: 200,
+                height: 100,
+                child: Image.asset('assets/chat.png'),
               ),
-              width: 200,
-              child: Image.asset('assets/chat.png'),
-            ),
-            Card(
-              margin: const EdgeInsets.all(20),
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: Form(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        TextFormField(
-                          key: const ValueKey('email'),
-                          keyboardType: TextInputType.emailAddress,
-                          decoration: const InputDecoration(
-                            labelText: 'Email Address',
+              Card(
+                margin: const EdgeInsets.all(20),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Form(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          TextFormField(
+                            key: const ValueKey('email'),
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: const InputDecoration(
+                              labelText: 'Email Address',
+                            ),
+                            autocorrect: false,
+                            textCapitalization: TextCapitalization.none,
+                            autofocus: true,
                           ),
-                          autocorrect: false,
-                          textCapitalization: TextCapitalization.none,
-                          autofocus: true,
-                        ),
-                      ],
+                          TextFormField(
+                            key: const ValueKey('password'),
+                            keyboardType: TextInputType.visiblePassword,
+                            decoration: InputDecoration(
+                              labelText: 'Password',
+                              suffix: passwordshown == false
+                                  ? IconButton(
+                                      icon: const Icon(Icons.visibility_off),
+                                      onPressed: () {
+                                        setState(() {
+                                          passwordshown = true;
+                                        });
+                                      },
+                                    )
+                                  : IconButton(
+                                      icon: const Icon(Icons.visibility),
+                                      onPressed: () {
+                                        setState(() {
+                                          passwordshown = false;
+                                        });
+                                      },
+                                    ),
+                            ),
+                            obscureText: passwordshown ? false : true,
+                            autocorrect: false,
+                            textCapitalization: TextCapitalization.none,
+                            autofocus: true,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
-        )),
+            ],
+          )),
+        ),
       ),
     );
   }
