7f741d8737ca57e35d02387494c740d59af7f869
sajal <sajalsahu8@gmail.com>
2023-07-24 05:47:37 +0400

    imagepicker widget

--- lib/widgets/user_image_picker.dart
+++ lib/widgets/user_image_picker.dart
@@ -1,27 +1,43 @@
+import "dart:io";
+
 import "package:firebase_auth/firebase_auth.dart";
 import "package:flutter/material.dart";
+import "package:image_picker/image_picker.dart";
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
 
   @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+
+  void _pickimage(){
+    
+    ImagePicker().pickImage(source: ImageSource.gallery);
+
+  }
+  @override
   Widget build(context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Flutter Chat"),
-        actions: [
-          IconButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            icon: const Icon(Icons.logout),
-            color: Theme.of(context).colorScheme.primary,
+    return Column(
+      children: [
+        const CircleAvatar(
+          radius: 40,
+          backgroundColor: Colors.grey,
+          foregroundImage: ,
+        ),
+        TextButton.icon(
+          onPressed: () {},
+          icon: const Icon(Icons.image),
+          label: Text(
+            "Add Image",
+            style: TextStyle(
+              color: Theme.of(context).primaryColor,
+            ),
           ),
-        ],
-      ),
-      body: const Center(
-        child: Text("loggedIn!"),
-      ),
+        ),
+      ],
     );
   }
 }
