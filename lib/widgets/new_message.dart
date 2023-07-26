f1e448c2fed10eb25ecbdf7b2587e5a94795e941
sajal <sajalsahu8@gmail.com>
2023-07-26 04:18:36 +0400

    newmsgfile

---
+++ lib/widgets/new_message.dart
@@ -0,0 +1,14 @@
+import "package:flutter/material.dart";
+
+class NewMessage extends StatefulWidget {
+  const NewMessage({super.key});
+  @override
+  State<NewMessage> createState() => _NewMessageState();
+}
+
+class _NewMessageState extends State<NewMessage> {
+  @override
+  Widget build(context) {
+    return Container();
+  }
+}
