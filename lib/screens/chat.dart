db0208f921564761375a4e30953f62a7a1be3054
sajal <sajalsahu8@gmail.com>
2023-07-27 18:52:41 +0400

    init

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,10 +1,22 @@
 import "package:chatapp/widgets/chat_messages.dart";
 import "package:chatapp/widgets/new_message.dart";
 import "package:firebase_auth/firebase_auth.dart";
+import "package:firebase_messaging/firebase_messaging.dart";
 import "package:flutter/material.dart";
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void init() {
+    super.initState();
+    FirebaseMessaging.instance.requestPermission();s
+  }
+
 
   @override
   Widget build(context) {
